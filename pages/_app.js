ac3edf39c566b567458e72cd52fa50a64a0eb39e
Abhinavpatel <121427766+TheVeganAbhinav@users.noreply.github.com>
2023-02-08 22:25:18 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import '../styles/globals.css';
+import { useState } from "react";
+import Wizard, { WizardContext } from "../components/wizard";
 import 'prismjs/themes/prism-tomorrow.css';
 
+
 function MyApp({ Component, pageProps }) {
+  const [data, setData] = useState({
+    // font defaults
+    "--font-primary": "sans-serif",
+    "--font-secondary": "sans-serif",
+  });
+
   return (
-    <>
-      <span className="theme-bejamas" />
-      <Component {...pageProps} />
-    </>
+    <WizardContext.Provider value={{ data, setData }}>
+      <Wizard page={<Component {...pageProps} globalData={data} />} />
+    </WizardContext.Provider>
   );
 }
 
 export default MyApp;
+
+
+
